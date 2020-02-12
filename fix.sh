0ce5662189360cb7b02239c8b9a9adfd4a659f38
Hongji Zhu <fireyoucan@gmail.com>
2020-02-12 20:48:02 +0800

    Add fix.sh

---
+++ fix.sh
@@ -0,0 +1,9 @@
+#########################################################################
+# Author: Hongji Zhu
+# Created On: 2020年02月10日 星期一 01时43分29秒
+#########################################################################
+#!/bin/bash
+
+python ./modules/python/src2/gen2.py \
+    ./build/modules/python_bindings_generator \
+    ./build/modules/python_bindings_generator/headers.txt
